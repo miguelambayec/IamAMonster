70335dc8c87bcbffffbf1752de5e1aecc8e1dac2
migz627 <ambayec.miguel27@gmail.com>
2022-08-15 17:29:51 +0800

    change monster animation to animation tree

--- Scripts/MonsterAnimationController.gd
+++ Scripts/MonsterAnimationController.gd
@@ -2,15 +2,34 @@ extends Node
 
 export(NodePath) var player_path
 onready var player := get_node(player_path)
-onready var animation := get_parent().get_node("Model/AnimationPlayer")
+onready var anim_tree := $"../AnimationTree"
+onready var anim_player := $"../Model/AnimationPlayer"
+
+var loop_animations = ["Idle", "Walk", "Run"]
+var idle_walk_blend = 0.0
+var walk_run_blend = 0.0
+
+func _ready():
+	setup_animations()
 
 func _physics_process(_delta):
+	
 	if player.is_attacking():
-		animation.play("Attack")
+		anim_tree["parameters/AttackOneShot/active"] = true
 	elif player.is_moving():
+		idle_walk_blend = lerp(idle_walk_blend, 1.0, 0.25)
+		
 		if player.is_running():
-			animation.play("Run")
+			walk_run_blend = 1.0
 		else:
-			animation.play("Walk")
+			walk_run_blend = 0.0
 	else:
-		animation.play("Idle")
+		idle_walk_blend = lerp(idle_walk_blend, 0.0, 0.25)
+		
+	anim_tree["parameters/IdleWalkBlend/blend_amount"] = idle_walk_blend
+	anim_tree["parameters/WalkRunBlend/blend_amount"] = walk_run_blend
+
+func setup_animations():
+	for anim_name in loop_animations:
+		var anim = anim_player.get_animation(anim_name)
+		anim.loop = true
